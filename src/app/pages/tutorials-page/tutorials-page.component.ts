eb2de58b31e69d6d178c4bf94629b2f348c721ee
Lancast <angelprep92@gmail.com>
2025-07-02 15:36:24 -0600

    feat:tutorialpages

---
+++ src/app/pages/tutorials-page/tutorials-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tutorials-page',
+  imports: [],
+  templateUrl: './tutorials-page.component.html',
+  styleUrl: './tutorials-page.component.css'
+})
+export class TutorialsPageComponent {
+
+}
